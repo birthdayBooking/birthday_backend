b4db0237292c79bba7ff6244e09d2c04d302af5e
holeshit1234 <hominhdund@gmail.com>
2024-03-12 16:04:39 +0700

    fix login

--- models/account.js
+++ models/account.js
@@ -1,19 +1,40 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const accountSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, 'Please tell us your  name!']
+    firstName: {
+      type: String
+    },
+    lastName: {
+      type: String
     },
     username: {
-      type: String
+      type: String,
+      unique: true,
+      require: [true, 'Please provide your username']
     },
     password: {
       type: String,
+      required: [true, 'Please provide a password!'],
       minlength: 8,
       select: false
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password!'],
+      validate: {
+        validator: function(el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!'
+      }
+    },
+    name: {
+      type: String,
+      required: [true, 'Please tell us your  name!']
     },
     email: {
       type: String,
@@ -45,47 +66,50 @@ const accountSchema = new mongoose.Schema(
   }
 );
 
-// accountSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
+accountSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
 
-//   this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
-//   this.passwordConfirm = undefined;
-//   next();
-// });
+  this.passwordConfirm = undefined;
+  next();
+});
 
-// accountSchema.pre('save', function (next) {
-//   if (!this.isModified('password') || this.isNew) return next();
+accountSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
 
-//   // - 1 giây để đảm bảo rằng token được tạo ra sau khi password được thay đổi
-//   this.passwordChangedAt = Date.now() - 1000;
-//   next();
-// });
+  // - 1 giây để đảm bảo rằng token được tạo ra sau khi password được thay đổi
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
 
-// accountSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
-//   return await bcrypt.compare(candidatePassword, userPassword);
-// };
+accountSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
-// accountSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-//   if (this.passwordChangedAt) {
-//     const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+accountSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
 
-//     return JWTTimestamp < changedTimestamp;
-//   }
+    return JWTTimestamp < changedTimestamp;
+  }
 
-//   // False means NOT changed
-//   return false;
-// };
+  // False means NOT changed
+  return false;
+};
 
-// accountSchema.methods.createPasswordResetToken = function () {
-//   const resetToken = crypto.randomBytes(32).toString('hex');
+accountSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
 
-//   this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex'); //digest là để mã hóa chuỗi
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex'); //digest là để mã hóa chuỗi
 
-//   this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //10 phút
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //10 phút
 
-//   return resetToken;
-// };
+  return resetToken;
+};
 const Account = mongoose.model('Account', accountSchema);
 
 module.exports = Account;
