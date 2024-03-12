965a7f6fd3e0fe3596b19719fbe47f17d9410e01
Vu Anh Lam <vuanhlam000@gmail.com>
2024-03-12 11:10:01 +0700

    get monthly number of booking by year

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAnalyticsOfSystem, getPartyStats, getHighestBookingParty } = require('../controllers/dashboardController');
+const { getAnalyticsOfSystem, getPartyStats, getHighestBookingParty, getMonthlyBooking } = require('../controllers/dashboardController');
 const catchAsync = require('../utils/catchAsync');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.route('/').get(catchAsync(getAnalyticsOfSystem));
 router.route('/party').get(catchAsync(getPartyStats));
 router.route('/hot-party').get(catchAsync(getHighestBookingParty));
+router.route('/monthly-booking/:year').get(catchAsync(getMonthlyBooking));
 //   .post(dashboardController.createUser);
 
 // router
