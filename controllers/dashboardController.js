b50a581b96c32acc87b1087e78a555b7381299a9
Vu Anh Lam <vuanhlam000@gmail.com>
2024-03-12 08:30:31 +0700

    get hotest booking party

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,4 +1,5 @@
 const Account = require('../models/account');
+const Order = require('../models/orderModel');
 const Party = require('../models/partyModel');
 
 const getAnalyticsOfSystem = async (req, res, next) => {
@@ -39,7 +40,35 @@ const getPartyStats = async (req, res, next) => {
   });
 };
 
+const getHighestBookingParty = async (req, res, next) => {
+  const partyStats = await Order.aggregate([
+    {
+      $group: {
+        _id: '$partyId',
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $sort: {
+        count: -1
+      }
+    },
+    {
+      $limit: 1
+    }
+  ]);
+
+  const { _id } = partyStats[0];
+
+  const party = await Party.findById(_id).lean()
+
+  res.status(200).json({
+    party
+  });
+};
+
 module.exports = {
   getAnalyticsOfSystem,
-  getPartyStats
+  getPartyStats,
+  getHighestBookingParty
 };
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getAnalyticsOfSystem, getPartyStats } = require('../controllers/dashboardController');
+const { getAnalyticsOfSystem, getPartyStats, getHighestBookingParty } = require('../controllers/dashboardController');
 const catchAsync = require('../utils/catchAsync');
 
 const router = express.Router();
 
 router.route('/').get(catchAsync(getAnalyticsOfSystem));
-router.route('/party').get(catchAsync(getPartyStats))
+router.route('/party').get(catchAsync(getPartyStats));
+router.route('/hot-party').get(catchAsync(getHighestBookingParty));
 //   .post(dashboardController.createUser);
 
 // router
