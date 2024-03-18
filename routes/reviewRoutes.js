76496aae42c654c360741a0d3e98bdd9ae00972e
holeshit1234 <hominhdund@gmail.com>
2024-03-18 19:39:49 +0700

    fix route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,11 +3,8 @@ const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 
-router
-    .get('/party/:partyId/reviews', reviewController.getPartyReviews);
+router.get('/party/:partyId/reviews', reviewController.getPartyReviews);
 
-router
-    .post('/party/:partyId/reviews', reviewController.createReview);
-router
-    .delete('/party/:partyId/reviews/:reviewId', reviewController.deleteReview);
+router.post('/reviews', reviewController.makeReview);
+router.delete('/party/:partyId/reviews/:reviewId', reviewController.deleteReview);
 module.exports = router;
