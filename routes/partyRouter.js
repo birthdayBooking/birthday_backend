a8b105e5ecca7ee14b1ce2c28d6655d2a94cd921
Đức Anh <ss3xneo@gmail.com>
2024-03-18 07:45:15 +0700

    add api party

--- routes/partyRouter.js
+++ routes/partyRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { createParty, getAllParties, getPartyInfo, updateParty, deleteParty } = require('../controllers/partyController');
+const { createParty, getAllParties, getPartyInfo, updateParty, deleteParty, getPartyByCategory } = require('../controllers/partyController');
 //const partyController = require('../controllers/partyController');
 const router = express.Router();
 
 router.route('/')
   .get(getAllParties)
   .post(createParty);
-
+router.route('/:categoryName')
+  .get(getPartyByCategory)
 router.route('/details/:partyId')
   .get(getPartyInfo)
   .patch(updateParty)
